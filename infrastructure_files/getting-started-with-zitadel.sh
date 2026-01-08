7cfef377a3be89a98090e6b866c0b6f6683b2293
Corey W <williamscorey384@gmail.com>
2026-01-07 19:13:02 -0500

    Update getting-started-with-zitadel.sh to use up to date version.
    Current version of Zitadel being used is pinned at v2.64.1.
    This is quite a bit behind the as of today latest version 4.9.0 (https://github.com/zitadel/zitadel/releases/tag/v4.9.0)

--- infrastructure_files/getting-started-with-zitadel.sh
+++ infrastructure_files/getting-started-with-zitadel.sh
@@ -906,7 +906,7 @@ services:
   zitadel:
     restart: 'always'
     networks: [netbird]
-    image: 'ghcr.io/zitadel/zitadel:v2.64.1'
+    image: 'ghcr.io/zitadel/zitadel:latest'
     command: 'start-from-init --masterkeyFromEnv --tlsMode $ZITADEL_TLS_MODE'
     env_file:
       - ./zitadel.env
